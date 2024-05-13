2a62112aaedd7ee0c1f36ddc531f71237f195316
Asato Ōi <83337845+asato-oi@users.noreply.github.com>
2024-05-14 00:46:32 +0900

    ⚡️ 講義内容復習

--- src/01/index.js
+++ src/01/index.js
@@ -1,6 +1,8 @@
 "use strict";
 
 import * as THREE from "three";
+import { OrbitControls } from "./OrbitControls";
+
 class ThreeApp {
   /**
    * カメラ定義
@@ -24,18 +26,48 @@ class ThreeApp {
   };
 
   /**
+   * 平行光源のパラメーター
+   */
+  static DIRECTIONAL_LIGHT_PARAM = {
+    color: 0xfffffff,
+    intensity: 1.0,
+    position: new THREE.Vector3(1.0, 1.0, 1.0),
+  };
+
+  /**
+   * アンビエントライトのパラメーター
+   */
+  static AMBIENT_LIGHT_PARAM = {
+    color: 0xffffff,
+    intensity: 0.1,
+  };
+
+  /**
    * マテリアルのパラメーター
    */
   static MATERIAL_PARAM = {
-    color: 0x3399ff, // マテリアルの基本色
+    color: 0xffffff, // マテリアルの基本色
+  };
+
+  /**
+   * ヘルパーのパラメーター
+   */
+  static HELPER_PARAM = {
+    axesHelper: 5.0,
   };
 
   renderer; // レンダラ
   scene; // シーン
   camera; // カメラ
+  directionalLight; // 平行光源（ディレクショナルライト）
+  ambientLight; // 環境光（アンビエントライト）
   geometry; // ジオメトリ
   material; // マテリアル
   box; // ボックスメッシュ
+  controls; //オービットコントロール
+  axesHelper; // 軸ヘルパー
+
+  isDown = false; // キーの押下状態用フラグ
 
   /**
    * @constructor
@@ -66,19 +98,67 @@ class ThreeApp {
     this.camera.position.copy(ThreeApp.CAMERA_PARAM.position);
     this.camera.lookAt(ThreeApp.CAMERA_PARAM.lookAt);
 
-    // ジオメトリ
-    this.geometry = new THREE.BoxGeometry(1.0, 1.0, 1.0);
+    // ディレクショナルライト
+    this.directionalLight = new THREE.DirectionalLight(
+      ThreeApp.DIRECTIONAL_LIGHT_PARAM.color,
+      ThreeApp.DIRECTIONAL_LIGHT_PARAM.intensity
+    );
+    this.directionalLight.position.copy(
+      ThreeApp.DIRECTIONAL_LIGHT_PARAM.position
+    );
+    this.scene.add(this.directionalLight);
 
-    // マテリアル
-    this.material = new THREE.MeshBasicMaterial(ThreeApp.MATERIAL_PARAM);
+    // アンビエントライト
+    this.ambientLight = new THREE.AmbientLight(
+      ThreeApp.AMBIENT_LIGHT_PARAM.color,
+      ThreeApp.AMBIENT_LIGHT_PARAM.intensity
+    );
+    this.scene.add(this.ambientLight);
+
+    // ジオメトリとマテリアル
+    this.geometry = new THREE.BoxGeometry(1.0, 1.0, 1.0);
+    this.material = new THREE.MeshPhongMaterial(ThreeApp.MATERIAL_PARAM);
 
     // メッシュ
     this.box = new THREE.Mesh(this.geometry, this.material);
-
     this.scene.add(this.box);
+
+    // ヘルパー
+    this.axesHelper = new THREE.AxesHelper(ThreeApp.HELPER_PARAM.axesHelper);
+    this.scene.add(this.axesHelper);
+
+    // オービットコントロール
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+
+    window.addEventListener("keydown", (e) => {
+      switch (e.key) {
+        case " ":
+          this.isDown = true;
+          break;
+        default:
+      }
+    });
+    window.addEventListener("keyup", () => {
+      this.isDown = false;
+    });
+    window.addEventListener("resize", () => {
+      this.renderer.setSize(window.innerWidth, window.innerHeight);
+      this.camera.aspect = window.innerWidth / window.innerHeight;
+      // カメラのパラメータが変更されたときは行列を更新する
+      // ※なぜ行列の更新が必要なのかについては、将来的にもう少し詳しく解説します
+      this.camera.updateProjectionMatrix();
+    });
   }
 
   render = () => {
+    requestAnimationFrame(this.render);
+
+    // 書かなくても大丈夫
+    this.controls.update();
+
+    if (this.isDown) {
+      this.box.rotation.y += 0.05;
+    }
     this.renderer.render(this.scene, this.camera);
   };
 }
